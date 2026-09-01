567113993bbc4dcde14fe43900deca7f1d90d7ac
tiramisu <>
2026-09-01 15:52:16 +0800

    fix bug

--- src/pages/CreatePostPage.tsx
+++ src/pages/CreatePostPage.tsx
@@ -1,4 +1,4 @@
-import { type FormEvent, useState } from "react";
+import { useState } from "react";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useNavigate } from "react-router-dom";
 import {
